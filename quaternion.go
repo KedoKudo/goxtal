f8f052ed7698dc7a33e253a24a655a7819c6e672
Chen <zdms1987@hotmail.com>
2017-11-30 13:57:58 -0500

    adding skeloton quanterion in Golang

---
+++ quaternion.go
@@ -0,0 +1,15 @@
+/*
+Quanternion calculation for manipulating cyxtal orientations
+rotations.
+*/
+
+package quanternion
+
+
+// q = w + Xi + Yj + Zk
+type Quanternion struct {
+	W  float64  \\ sin(theta/2)
+	X  float64  \\ vx
+	Y  float64  \\ vy
+	Z  float64  \\ vz
+}
---
+++ quaternion_test.go
@@ -0,0 +1,5 @@
+/*
+Testing for the quanternion packages
+*/
+
+package quanternion
