b88a179019e667683a5b5713fa81e7582da8f8cf
Chen <zdms1987@hotmail.com>
2017-12-01 11:51:07 -0500

    adding our first golang testing case

--- quaternion/quaternion_test.go
+++ quaternion/quaternion_test.go
@@ -3,3 +3,20 @@ Testing for the quanternion packages
 */
 
 package quaternion
+
+import (
+	"testing"
+)
+
+var (
+	q00 = Quaternion{W: 1, X: 0, Y: 0, Z: 0}
+	q01 = Quaternion{W: 5, X: 0, Y: 0, Z: 0}
+	s00 = 5.0
+)
+
+func TestScale(t *testing.T) {
+	q00.Scale(s00)
+	if q00 != q01 {
+		t.Fail()
+	}
+}
