5890db5a5b688611efb59ef767efb5552be8f3cb
Chen <zdms1987@hotmail.com>
2017-12-01 11:31:47 -0500

    add empty packages for potential useful math

---
+++ xtalMath/xtalMath.go
@@ -0,0 +1,9 @@
+/*
+xtalMath package contains some numerical operations handy for xtal related calculations
+*/
+
+package xtalMath
+
+import (
+	"math"
+)
---
+++ xtalMath/xtalMath_test.go
@@ -0,0 +1,7 @@
+/*
+package test for xtalMath
+*/
+
+import (
+	"xtalMath"
+)
